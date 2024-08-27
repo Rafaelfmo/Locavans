06f14dc72fe3a76cc9e73d67314d576eabe52d40
Rafaelfmo <rafaelfmo11@gmail.com>
2024-08-27 17:17:46 -0300

    Add prettier plugin

--- src/components/capa.tsx
+++ src/components/capa.tsx
@@ -3,21 +3,21 @@ import { FaWhatsapp } from "react-icons/fa";
 export function Capa() {
   return (
     <section
-      className="relative h-screen bg-cover bg-center flex items-center justify-center"
+      className="relative flex h-screen items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/banner.png')" }}
     >
       <div className="absolute inset-0 bg-black opacity-30"></div>
-      <div className="relative z-10 text-center text-white px-4">
-        <h1 className="text-3xl md:text-4xl font-bold mb-4 text-orange-400">
+      <div className="relative z-10 px-4 text-center text-white">
+        <h1 className="mb-4 text-3xl font-bold text-orange-400 md:text-4xl">
           A MELHOR EM ALUGUEL E FRETAMENTO DE VANS PARA PASSEIOS, TRANSLADO E
           EXECUTIVO
         </h1>
-        <p className="text-lg md:text-4xl mb-6">
+        <p className="mb-6 text-lg md:text-4xl">
           Fale agora com um de nossos atendentes
         </p>
-        <button className="bg-orange-500 text-white px-6 py-3 rounded-full hover:bg-orange-600 transition text-lg">
-            <FaWhatsapp className="inline-block size-6 m-2" />
-            Fale com a gente
+        <button className="rounded-full bg-orange-500 px-6 py-3 text-lg text-white transition hover:bg-orange-600">
+          <FaWhatsapp className="m-2 inline-block size-6" />
+          Fale com a gente
         </button>
       </div>
     </section>
