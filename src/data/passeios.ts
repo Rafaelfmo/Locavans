40c9f37a338116ac98bf6052c6c71e70c79a4a77
Rafaelfmo <rafaelfmo11@gmail.com>
2025-04-11 13:07:30 -0300

    Add details to cards

--- src/data/passeios.ts
+++ src/data/passeios.ts
@@ -5,23 +5,18 @@ export interface Passeio {
   valor: number;
 }
 
-import lagoinha from "../assets/passeios/lagoinha.png";
 import canoaquebrada from "../assets/passeios/canoa.png";
-import morrobranco from "../assets/passeios/morrobranco.png";
+import fortim from "../assets/passeios/fortim.png";
+import paracuru from "../assets/passeios/paracuru.png";
+import cumbuco from "../assets/passeios/cumbuco.png";
+import lagoinha from "../assets/passeios/lagoinha.png";
+import aguasbelas from "../assets/passeios/aguasbelas.png";
 import beachpark from "../assets/passeios/beachpark.png";
-import jericoacara from "../assets/passeios/jericoacara.png";
+import flecheiras from "../assets/passeios/flecheiras.png";
+import engenhoca from "../assets/passeios/engenhoca.png";
+import flechau from "../assets/passeios/flechau.png";
 
 export const passeios: Passeio[] = [
-  {
-    imagem: lagoinha,
-    titulo: "Praia de Lagoinha",
-    caracteristicas: [
-      "Tipo: Privativo",
-      "Transporte: Ida e volta",
-      "Veículo: Hilux SW4",
-    ],
-    valor: 100,
-  },
   {
     imagem: canoaquebrada,
     titulo: "Praia de Canoa Quebrada",
@@ -29,18 +24,64 @@ export const passeios: Passeio[] = [
       "Tipo: Privativo",
       "Transporte: Ida e volta",
       "Veículo: Hilux SW4",
+      "Distância: 166 km",
     ],
-    valor: 200,
+    valor: 90,
   },
   {
-    imagem: morrobranco,
-    titulo: "Praia de Morro Branco",
+    imagem: fortim,
+    titulo: "Fortim",
     caracteristicas: [
-      "Tipo: Privativo e compartilhado",
+      "Tipo: Privativo",
       "Transporte: Ida e volta",
       "Veículo: Hilux SW4",
+      "Distância: 130 km",
     ],
-    valor: 300,
+    valor: 100,
+  },
+  {
+    imagem: paracuru,
+    titulo: "Paracuru",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 90 km",
+    ],
+    valor: 80,
+  },
+  {
+    imagem: cumbuco,
+    titulo: "Cumbuco",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 36 km",
+    ],
+    valor: 60,
+  },
+  {
+    imagem: lagoinha,
+    titulo: "Praia de Lagoinha",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 110 km",
+    ],
+    valor: 85,
+  },
+  {
+    imagem: aguasbelas,
+    titulo: "Águas Belas",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 60 km",
+    ],
+    valor: 75,
   },
   {
     imagem: beachpark,
@@ -49,17 +90,41 @@ export const passeios: Passeio[] = [
       "Tipo: Privativo e compartilhado",
       "Transporte: Ida e volta",
       "Veículo: Sprinter 415",
+      "Distância: 16 km",
     ],
-    valor: 400,
+    valor: 55,
   },
   {
-    imagem: jericoacara,
-    titulo: "Praia de Jericoacoara",
+    imagem: flecheiras,
+    titulo: "Flecheiras",
     caracteristicas: [
       "Tipo: Privativo",
       "Transporte: Ida e volta",
       "Veículo: Hilux SW4",
+      "Distância: 140 km",
     ],
-    valor: 500,
+    valor: 90,
+  },
+  {
+    imagem: engenhoca,
+    titulo: "Engenhoca Parque",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 16 km",
+    ],
+    valor: 100,
+  },
+  {
+    imagem: flechau,
+    titulo: "Flechaú",
+    caracteristicas: [
+      "Tipo: Privativo",
+      "Transporte: Ida e volta",
+      "Veículo: Hilux SW4",
+      "Distância: 130 km",
+    ],
+    valor: 180,
   },
 ];
--- src/sections/passeios.tsx
+++ src/sections/passeios.tsx
@@ -1,5 +1,27 @@
-import { FaUserTie, FaUserFriends, FaCheck, FaCar } from "react-icons/fa";
+import {
+  FaUserTie,
+  FaUserFriends,
+  FaCheck,
+  FaCar,
+  FaRoute,
+} from "react-icons/fa";
+
 import { passeios } from "../data/passeios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+// Promoção 3 praias em um dia
+import morrobranco from "../assets/passeios/morrobranco.png";
+import fontes from "../assets/passeios/fontes.png";
+import canoaquebrada from "../assets/passeios/canoa.png";
+
+// Imagens do passeio especial
+import lagoaParaiso from "../assets/passeios/lagoadoparaiso.png";
+import buracoAzul from "../assets/passeios/buracoazul.png";
+import arvorePreguica from "../assets/passeios/arvoredapreguica.png";
+import pedraFurada from "../assets/passeios/pedrafurada.png";
 
 export function Passeios() {
   const iconesCaracteristicas: { [key: string]: JSX.Element } = {
@@ -10,6 +32,14 @@ export function Passeios() {
     "Transporte: Ida e volta": <FaCheck className="mr-2 text-orange-500" />,
     "Veículo: Hilux SW4": <FaCar className="mr-2 text-orange-500" />,
     "Veículo: Sprinter 415": <FaCar className="mr-2 text-orange-500" />,
+    "Distância: 166 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 130 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 90 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 36 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 110 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 140 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 60 km": <FaRoute className="mr-2 text-orange-500" />,
+    "Distância: 16 km": <FaRoute className="mr-2 text-orange-500" />,
   };
 
   return (
@@ -17,6 +47,169 @@ export function Passeios() {
       <div className="container mx-auto max-w-7xl px-4 font-inter">
         <h2 className="mb-12 text-center text-4xl font-bold text-gray-800">
           Passeios pelo Nordeste
+        </h2>
+
+        {/* Promoção: Exibir 3 cards grandes lado a lado */}
+        <div className="mb-8 rounded-3xl bg-orange-500 p-8">
+          <h2 className="mb-8 text-center text-3xl font-semibold text-white">
+            Promoção: Visite 3 praias em um único dia!
+          </h2>
+          <div className="grid justify-center gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex flex-col items-center overflow-hidden rounded-3xl bg-black shadow-lg">
+              <div className="relative h-64 w-full">
+                <img
+                  src={morrobranco}
+                  alt="Morro Branco"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <span className="text-center text-2xl font-bold text-white">
+                  Morro Branco
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col items-center overflow-hidden rounded-3xl bg-black shadow-lg">
+              <div className="relative h-64 w-full">
+                <img
+                  src={fontes}
+                  alt="Praia das Fontes"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <span className="text-center text-2xl font-bold text-white">
+                  Praia das Fontes
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col items-center overflow-hidden rounded-3xl bg-black shadow-lg">
+              <div className="relative h-64 w-full">
+                <img
+                  src={canoaquebrada}
+                  alt="Canoa Quebrada"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <span className="text-center text-2xl font-bold text-white">
+                  Canoa Quebrada
+                </span>
+              </div>
+            </div>
+          </div>
+          <p className="mt-8 text-center text-xl font-bold text-white">
+            Apenas <span className="text-3xl">R$ 110,00</span> por pessoa
+          </p>
+          <div className="mt-4 flex justify-center">
+            <a
+              href="https://wa.me/+5585996237064?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20a%20promoção%20de%20visitar%203%20praias%20em%20um%20único%20dia."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="rounded-full bg-black px-6 py-3 text-white transition duration-300 hover:bg-gray-800">
+                Entre em contato
+              </button>
+            </a>
+          </div>
+        </div>
+
+        {/* Passeio Especial: 1 dia em Jeri */}
+        <div className="mb-8 rounded-3xl bg-orange-500 bg-gradient-to-r p-8 shadow-lg">
+          <h2 className="mb-8 text-center text-3xl font-semibold text-white">
+            Passeio Especial: 1 dia em Jericoacoara
+          </h2>
+          <Swiper
+            modules={[Navigation]}
+            navigation
+            spaceBetween={20}
+            slidesPerView={1}
+            className="rounded-lg"
+          >
+            <SwiperSlide>
+              <div className="relative h-96 w-full">
+                <img
+                  src={lagoaParaiso}
+                  alt="Lagoa do Paraíso"
+                  className="absolute inset-0 h-full w-full rounded-lg object-cover"
+                  loading="lazy"
+                  decoding="async"
+                  style={{ imageRendering: "auto" }}
+                />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
+                  <span className="text-xl font-semibold">
+                    Lagoa do Paraíso
+                  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative h-96 w-full">
+                <img
+                  src={buracoAzul}
+                  alt="Buraco Azul"
+                  className="absolute inset-0 h-full w-full rounded-lg object-cover"
+                  loading="lazy"
+                  decoding="async"
+                  style={{ imageRendering: "auto" }}
+                />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
+                  <span className="text-xl font-semibold">Buraco Azul</span>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative h-96 w-full">
+                <img
+                  src={arvorePreguica}
+                  alt="Árvore da Preguiça"
+                  className="absolute inset-0 h-full w-full rounded-lg object-cover"
+                  loading="lazy"
+                  decoding="async"
+                  style={{ imageRendering: "auto" }}
+                />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
+                  <span className="text-xl font-semibold">
+                    Árvore da Preguiça
+                  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative h-96 w-full">
+                <img
+                  src={pedraFurada}
+                  alt="Pedra Furada"
+                  className="absolute inset-0 h-full w-full rounded-lg object-cover"
+                  loading="lazy"
+                  decoding="async"
+                  style={{ imageRendering: "auto" }}
+                />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
+                  <span className="text-xl font-semibold">Pedra Furada</span>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+          <p className="mt-8 text-center text-2xl font-bold text-white">
+            Apenas <span className="text-3xl">R$ 290,00</span> por pessoa
+          </p>
+          <div className="mt-6 flex justify-center">
+            <a
+              href="https://wa.me/+5585996237064?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20o%20passeio%20especial%20de%201%20dia%20em%20Jeri."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="rounded-full bg-black px-8 py-4 text-lg font-semibold text-white transition duration-300 hover:bg-gray-800">
+                Reservar Passeio
+              </button>
+            </a>
+          </div>
+        </div>
+
+        {/* Lista de Passeios */}
+        <h2 className="mb-8 text-center text-2xl font-semibold text-gray-800">
+          Outros Passeios
         </h2>
         <div className="grid justify-center gap-8 md:grid-cols-2 lg:grid-cols-3">
           {passeios.map((passeio, index) => (
